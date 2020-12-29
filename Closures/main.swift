f899bd5e9f359fd589a7c13a30a996516e9c600d
João Victor Ipirajá <joaovictorpiraja@gmail.com>
2020-12-29 19:42:08 -0300

    Update main.swift

--- Closures/main.swift
+++ Closures/main.swift
@@ -5,7 +5,16 @@
 //  Created by João Victor Ipirajá de Alencar on 29/12/20.
 //
 
-import Foundation
 
-print("Hello, World!")
 
+func calculator (n1: Int, n2:Int, operation:(Int,Int) -> Int) -> Int{
+    return operation(n1,n2)
+}
+let result = calculator(n1: 2, n2: 3){$0*$1}
+
+print(result)
+
+let array = [6,2,3,9,4,1]
+
+let arrayPlusOne = array.map{$0+1}
+let arrayToString = array.map{"\($0)"}
